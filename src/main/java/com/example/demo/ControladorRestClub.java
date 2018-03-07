0aac813e0fe8b04b9bfce99f8dd63a9370ef78d8
davidbz3 <35699987+davidbz3@users.noreply.github.com>
2018-03-07 20:44:30 +0100

    Update ControladorRestClub.java

--- src/main/java/com/example/demo/ControladorRestClub.java
+++ src/main/java/com/example/demo/ControladorRestClub.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControlle; //rep
+impor org.springframework.web.bind.annotation.RestController; //rep
 // pasar el .json a objetos java 
 // de los clubs 
 @RestController
