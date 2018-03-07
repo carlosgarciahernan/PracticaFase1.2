49aa67b71dc0a3f0c7d06c458aafa171a61026dc
davidbz3 <35699987+davidbz3@users.noreply.github.com>
2018-03-07 20:49:59 +0100

    Update ControladorRestJugador.java

--- src/main/java/com/example/demo/ControladorRestJugador.java
+++ src/main/java/com/example/demo/ControladorRestJugador.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springtramework.web.bind.annotation.PathVariable:						//rep
+// paso de json a objeto java
+// Jugadores
 
 @RestController
 public class ControladorRestJugador {
@@ -18,6 +21,7 @@ public class ControladorRestJugador {
 	public List<Jugador> getJugadores(){
 		return repositorio_de_jugadores.findAll();
 	}
+	
 	
 	@RequestMapping(value="/API/jugadores/{JugadorName}", method = RequestMethod.GET)
 	public List<Jugador> getJugador(@PathVariable("JugadorName") String JugadorName) {
